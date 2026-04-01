46ac6e8f18e6b7453c794fbeaf8267c71bcd5c0a
Lucas Esperança Stein <m98499@ACAD.UNISC.BR>
2026-04-01 20:09:22 -0300

    Springboot Adicionado

---
+++ src/main/java/com/example/socialmusic/SocialmusicApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/socialmusic/SocialmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
